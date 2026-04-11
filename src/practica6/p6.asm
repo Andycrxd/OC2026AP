f52640bf46ed66dd64098a3461bf36fc4e92555f
Andy <andypalomares40@gmail.com>
2026-04-11 20:19:21 +0000

    salvar cambios

--- src/practica6/p6.asm
+++ src/practica6/p6.asm
@@ -1,86 +1,168 @@
-%include "../../lib/pc_io.inc"  	; incluir declaraciones de procedimiento externos
-								; que se encuentran en la biblioteca libpc_io.a
+%include "../../lib/pc_io.inc"
 
-section	.text
-	global _start       ;referencia para inicio de programa
-	
-_start:                   
+section .text
+global _start
 
-    call getch
-    call itoa
-    mov edx,ncad
+_start:
+
+    ; Mostrar mensaje
+    mov edx, msg1
     call puts
 
-    
-
-    mov bx,word[len]
-    mov edx,cad
+    ; Capturar cadena
+    mov bl, [len]      ; tamaño máximo
+    mov edx, cad       ; dirección de la cadena
     call capturar
-    mov al,[nlin]
+
+    ; Salto de línea
+    mov al, [nlin]
     call putchar
+
+    ; Mostrar ORIGINAL
+    mov edx, msg2
+    call puts
+    mov edx, cad
     call puts
 
-    mov al,[nlin]
+    ; MAYÚSCULAS
+    mov edx, cad
+    call mayusculas
+
+    mov edx, msg3
+    call puts
+    mov edx, cad
+    call puts
+
+    ; MINÚSCULAS
+    mov edx, cad
+    call minusculas
+
+    mov edx, msg4
+    call puts
+    mov edx, cad
+    call puts
+
+    ; Salida del programa
+    mov eax, 1
+    int 0x80
+
+
+; ================================
+; PROCEDIMIENTO: CAPTURAR
+; ================================
+capturar:
+    push edx
+    push cx
+
+    mov cx, bx
+    dec cx
+
+.ciclo:
+    call getch
+    cmp al, 127
+    jne .guardar
+
+    call borrar
+    jmp .ciclo
+
+.guardar:
     call putchar
+    mov [edx], al
+
+    cmp al, 0xA   ; Enter
+    je .salir
+
+    inc edx
+    loop .ciclo
+
+.salir:
+    mov byte [edx], 0
+
+    pop cx
+    pop edx
+    ret
+
+
+; ================================
+; BORRAR (backspace)
+; ================================
+borrar:
+    push ax
+
+    mov al, 0x8
+    call putchar
+    mov al, ' '
+    call putchar
+    mov al, 0x8
     call putchar
 
-	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
-	int	0x80        	; llamada al sistema - fin de programa
-
-    capturar:
-        push edx
-        push cx
-        mov cx,bx
-        dec cx
-    .ciclo: 
-        call getch
-        cmp al,127
-        jne .guardar
-        call borrar
-        jmp .ciclo
-       .guardar:
-        call putchar
-        mov [edx],al
-        cmp al,0xa
-        je .salir
-        inc edx
-        loop .ciclo
-
-        .salir:
-        mov byte[edx],0
-        pop cx
-        pop edx
-        ret
-
-    borrar:
-        push ax 
-        mov al,0x8
-        call putchar    
-        mov al,' '
-        call putchar
-        mov al,0x8
-        call putchar   
-        pop ax
-        ret 
-
-    itoa:
-        push bx
-        mov bl,100
-        mov ah,0
-        div bl
-        mov bx,ax
-        add al,'0'
-        call putchar
-        mov al,ah
-        add al,'0'
-        call putchar
+    pop ax
+    ret
 
 
+; ================================
+; MAYÚSCULAS
+; ================================
+mayusculas:
+    push edx
 
-        pop bx
-        ret
-section	.data
-    ncad db 0xa,'Cadena: ',0
-    nlin db 0xa
+.recorrer:
+    mov al, [edx]
+    cmp al, 0
+    je .fin
+
+    cmp al, 'a'
+    jl .sig
+    cmp al, 'z'
+    jg .sig
+
+    sub al, 32
+    mov [edx], al
+
+.sig:
+    inc edx
+    jmp .recorrer
+
+.fin:
+    pop edx
+    ret
+
+
+; ================================
+; MINÚSCULAS
+; ================================
+minusculas:
+    push edx
+
+.recorrer:
+    mov al, [edx]
+    cmp al, 0
+    je .fin
+
+    cmp al, 'A'
+    jl .sig
+    cmp al, 'Z'
+    jg .sig
+
+    add al, 32
+    mov [edx], al
+
+.sig:
+    inc edx
+    jmp .recorrer
+
+.fin:
+    pop edx
+    ret
+
+
+section .data
+    msg1 db "Ingresa una cadena: ",0
+    msg2 db 0xA,"Original: ",0
+    msg3 db 0xA,"Mayusculas: ",0
+    msg4 db 0xA,"Minusculas: ",0
+
+    nlin db 0xA
     len db 64
-    cad	times 64 db 0
+
+    cad times 64 db 0
