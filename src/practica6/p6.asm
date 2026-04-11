03f3ed98d5debb010e2779f224e303a3b15e28ec
Andy <andypalomares40@gmail.com>
2026-04-11 21:38:00 +0000

    salvar cambios

--- src/practica6/p6.asm
+++ src/practica6/p6.asm
@@ -5,25 +5,162 @@ global _start
 
 _start:
 
+    ; Mensaje inicial
+    mov edx, msg1
+    call puts
+
+    ; Capturar cadena
+    mov bl, [len]
     mov edx, cad
+    call capturar
 
-ciclo:
-    mov al, [edx]   ; leer letra
-    cmp al, 0       ; ¿terminó?
-    je fin
+    ; Salto de línea
+    mov al, [nlin]
+    call putchar
 
-    call putchar    ; imprimir letra
+    ; ORIGINAL
+    mov edx, msg2
+    call puts
+    mov edx, cad
+    call puts
 
-    inc edx         ; siguiente letra
-    jmp ciclo
+    ; MAYÚSCULAS
+    mov edx, cad
+    call mayusculas
+    mov edx, msg3
+    call puts
+    mov edx, cad
+    call puts
 
-fin:
-    mov edx,msg
-    call puts 
+    ; MINÚSCULAS
+    mov edx, cad
+    call minusculas
+    mov edx, msg4
+    call puts
+    mov edx, cad
+    call puts
+
+    ; Salida correcta
     mov eax, 1
     mov ebx, 0
     int 0x80
 
+
+; ============================
+; CAPTURAR
+; ============================
+capturar:
+    push edx
+    push cx
+
+    mov cx, bx
+    dec cx
+
+.ciclo:
+    call getch
+
+    cmp al, 127
+    jne .verificar
+    call borrar
+    jmp .ciclo
+
+.verificar:
+    cmp al, 0xA      ; ENTER
+    je .salir
+
+    call putchar
+    mov [edx], al
+    inc edx
+    loop .ciclo
+
+.salir:
+    mov byte [edx], 0
+
+    pop cx
+    pop edx
+    ret
+
+
+; ============================
+; BORRAR
+; ============================
+borrar:
+    push ax
+
+    mov al, 0x8
+    call putchar
+    mov al, ' '
+    call putchar
+    mov al, 0x8
+    call putchar
+
+    pop ax
+    ret
+
+
+; ============================
+; MAYÚSCULAS
+; ============================
+mayusculas:
+    push edx
+
+.recorrer:
+    mov al, [edx]
+    cmp al, 0
+    je .fin
+
+    cmp al, 'a'
+    jl .sig
+    cmp al, 'z'
+    jg .sig
+
+    sub al, 32
+    mov [edx], al
+
+.sig:
+    inc edx
+    jmp .recorrer
+
+.fin:
+    pop edx
+    ret
+
+
+; ============================
+; MINÚSCULAS
+; ============================
+minusculas:
+    push edx
+
+.recorrer:
+    mov al, [edx]
+    cmp al, 0
+    je .fin
+
+    cmp al, 'A'
+    jl .sig
+    cmp al, 'Z'
+    jg .sig
+
+    add al, 32
+    mov [edx], al
+
+.sig:
+    inc edx
+    jmp .recorrer
+
+.fin:
+    pop edx
+    ret
+
+
 section .data
-    cad db "hola",0
-    msg db "",0xA,0
+    msg1 db "Ingresa una cadena: ",0
+    msg2 db 0xA,"Original: ",0
+    msg3 db 0xA,"Mayusculas: ",0
+    msg4 db 0xA,"Minusculas: ",0
+
+    nlin db 0xA
+    len db 64
+
+    cad times 64 db 0
