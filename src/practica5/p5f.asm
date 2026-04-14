f255a6f09402b23389f6c69954426d76e783381d
Andy <andypalomares40@gmail.com>
2026-04-14 21:16:49 +0000

    salvar cambios

--- src/practica5/p5f.asm
+++ src/practica5/p5f.asm
@@ -13,8 +13,8 @@ _start:
 
     ;mensaje ya cambiado
     mov ebx, msg
-    mov esi, 19
-    mov byte [ebx + esi*1], '%'
+    mov esi, 14
+    mov byte [ebx + esi*1+5], '%'
 
 
     ;impriem la devuelta el mensaje ya cambiado
