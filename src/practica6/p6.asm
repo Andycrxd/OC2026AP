c07c63898e8dd0a60859a6dd6c1b5a70483f3b98
Andy <andypalomares40@gmail.com>
2026-04-18 23:50:53 +0000

    salvar cambios

--- src/practica6/p6.asm
+++ src/practica6/p6.asm
@@ -7,25 +7,26 @@ _start:
 
     ; Mensaje inicial
     mov edx, msg1
-    call puts
+    call puts              ;funcioon para imprimir edx
 
     ; Capturar cadena
-    mov bl, [len]
-    mov edx, cad
-    call capturar
+    mov bl, [len]         ; asignamos bytes a bl
+    mov bh,0               ; asignamos cero para que no alla problemas despues
+    mov edx, cad            ;le asignamos 64 espacios vacios
+    call capturar       
 
     ; Salto de línea
     mov al, [nlin]
     call putchar
 
     ; ORIGINAL
-    mov edx, msg2
-    call puts
-    mov edx, cad
-    call puts
+    mov edx, msg2     ; asigna la cadena
+    call puts          ; la muestra 
+    mov edx, cad        ;   edx apunta al inicio por que en capturar no lo moviste edx y cad solo es la direccion incial
+    call puts     ; imprime edx
 
     ; MAYÚSCULAS
-    mov edx, cad
+    mov edx, cad   ; mueve edx a la direccion iniciar
     call mayusculas
     mov edx, msg3
     call puts
@@ -40,85 +41,100 @@ _start:
     mov edx, cad
     call puts
 
+    ; Salto de líne pra que se vea mejor
+    mov al, [nlin]
+    call putchar
+    
     ; Salida correcta
     mov eax, 1
     mov ebx, 0
     int 0x80
 
 
-; ============================
+; ============================================================================
 ; CAPTURAR
-; ============================
+
 capturar:
     push edx
     push cx
     push esi
 
-    mov esi,0
-    mov cx, bx
-    dec cx
+    mov esi, 0
+    mov cx, bx   ; en bx esta dentro   esta bh y bl osea 64 bytes de bl
+    dec cx      ; decrementamos para el espacio de 0
 
 .ciclo:
     call getch
 
-    cmp al, 127
-    jne .verificar
+    cmp al, 127  ; pregunta si son iguales 
+    jne .verificar  ;si son iguaes no entra a verificar
+
+    cmp esi, 0          ; ¿ya estamos al inicio?
+    je .ciclo           ; no borrar
+
     call borrar
+    dec esi             ; retroceder en el buffer
     jmp .ciclo
 
 .verificar:
-    cmp al, 0xA      ; ENTER
+    cmp al, 0xA         ; si es un ENTER entra en salir
     je .salir
 
-    call putchar
+    call putchar    ; muestra  la letrra 
     mov [edx+esi], al
-    
     inc esi
     loop .ciclo
 
 .salir:
-    mov byte [edx], 0
-    
+    mov byte [edx+esi], 0   ; terminar cadena donde realmente acabó
+
     pop esi
     pop cx
     pop edx
     ret
 
 
-; ============================
+; CAPTURAR fin
+; ============================================================================
+
+; ============================================================================
 ; BORRAR
-; ============================
+
 borrar:
     push ax
 
-    mov al, 0x8
-    call putchar
-    mov al, ' '
-    call putchar
-    mov al, 0x8
-    call putchar
+    mov al, 0x8   ; backspace
+    call putchar   ; ahora esta el cursor posicion ala izq
+    mov al, ' '    ;  cambia donde  esta el cursor a vacio
+    call putchar   ; imprime y desaparece el valor 
+    mov al, 0x8    ; otro bakspace 
+    call putchar   ; ahora el cursor esta a la izq
 
     pop ax
     ret
 
+; BORRAR FIN
+; ============================================================================
 
-; ============================
+
+
+; ============================================================================
 ; MAYÚSCULAS
-; ============================
+
 mayusculas:
-    push edx
+    push edx  ; aguarda la captura
 
 .recorrer:
     mov al, [edx]
-    cmp al, 0
+    cmp al, 0         ; al == 0 entra  a fin
     je .fin
 
-    cmp al, 'a'
-    jl .sig
-    cmp al, 'z'
-    jg .sig
+    cmp al, 'a'  ; al < a
+    jl .sig      ; salta si es menor
+    cmp al, 'z'   ;  al > z
+    jg .sig        ; salta si es mayor
 
-    sub al, 32
+    sub al, 32  ; convertir a mayuscula 
     mov [edx], al
 
 .sig:
@@ -130,9 +146,14 @@ mayusculas:
     ret
 
 
-; ============================
+
+; MAYÚSCULAS FIN 
+; ============================================================================
+
+
+; ============================================================================
 ; MINÚSCULAS
-; ============================
+
 minusculas:
     push edx
 
@@ -158,9 +179,15 @@ minusculas:
     ret
 
 
+
+; MINÚSCULAS FIN
+; ============================================================================
+
+
+
 section .data
     msg1 db "Ingresa una cadena: ",0
-    msg2 db 0xA,"Original: ",0
+    msg2 db 0xA,"Cadena ingresada: ",0
     msg3 db 0xA,"Mayusculas: ",0
     msg4 db 0xA,"Minusculas: ",0
 
