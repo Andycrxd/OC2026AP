0323a74a55e3e5a2be76128e46396d69d9ab6e73
Andy <andypalomares40@gmail.com>
2026-04-18 06:13:35 +0000

    salvar cambios

--- src/practica/p7.asm
+++ src/practica/p7.asm
@@ -1,71 +1,174 @@
 %include "../../lib/pc_io.inc"
 
 section .text
-    global _start
+global _start
 
 _start:
+
+    ; Mensaje inicial
     mov edx, msg1
     call puts
 
+    ; Capturar cadena
+    mov bl, [len]
     mov edx, cad
-    mov ax, 64
-    call Capturar
+    call capturar
 
+    ; Salto de línea
+    mov al, [nlin]
+    call putchar
+
+    ; ORIGINAL
+    mov edx, msg2
+    call puts
     mov edx, cad
     call puts
 
+    ; MAYÚSCULAS
+    mov edx, cad
+    call mayusculas
+    mov edx, msg3
+    call puts
+    mov edx, cad
+    call puts
+
+    ; MINÚSCULAS
+    mov edx, cad
+    call minusculas
+    mov edx, msg4
+    call puts
+    mov edx, cad
+    call puts
+
+    ; Salida correcta
     mov eax, 1
-    xor ebx, ebx
+    mov ebx, 0
     int 0x80
 
-Capturar:
+
+; ============================
+; CAPTURAR
+; ============================
+capturar:
     push edx
     push cx
+    push esi
 
-    mov cx, ax
+    mov esi, 0
+    mov cx, bx
     dec cx
-    mov esi, edx
 
 .ciclo:
-    call getche
-
-    cmp al, 0xA
-    je .fin
+    call getch
 
     cmp al, 127
-    je .borrar
+    jne .verificar
 
-    cmp cx, 0
-    je .ciclo
+    cmp esi, 0          ; ¿ya estamos al inicio?
+    je .ciclo           ; no borrar
 
-    mov [edx], al
-    inc edx
-    dec cx
+    call borrar
+    dec esi             ; retroceder en el buffer
     jmp .ciclo
 
-.borrar:
-    cmp edx, esi
-    je .ciclo
+.verificar:
+    cmp al, 0xA         ; ENTER
+    je .salir
 
-    dec edx
-    inc cx
-
-    mov al, 8
     call putchar
-    mov al, ' '
-    call putchar
-    mov al, 8
-    call putchar
+    mov [edx+esi], al
+    inc esi
+    loop .ciclo
 
-    jmp .ciclo
+.salir:
+    mov byte [edx+esi], 0   ; terminar cadena donde realmente acabó
 
-.fin:
-    mov byte [edx], 0
+    pop esi
     pop cx
     pop edx
     ret
 
+
+; ============================
+; BORRAR
+; ============================
+borrar:
+    push ax
+
+    mov al, 0x8
+    call putchar
+    mov al, ' '
+    call putchar
+    mov al, 0x8
+    call putchar
+
+    pop ax
+    ret
+
+
+; ============================
+; MAYÚSCULAS
+; ============================
+mayusculas:
+    push edx
+
+.recorrer:
+    mov al, [edx]
+    cmp al, 0
+    je .fin
+
+    cmp al, 'a'
+    jl .sig
+    cmp al, 'z'
+    jg .sig
+
+    sub al, 32
+    mov [edx], al
+
+.sig:
+    inc edx
+    jmp .recorrer
+
+.fin:
+    pop edx
+    ret
+
+
+; ============================
+; MINÚSCULAS
+; ============================
+minusculas:
+    push edx
+
+.recorrer:
+    mov al, [edx]
+    cmp al, 0
+    je .fin
+
+    cmp al, 'A'
+    jl .sig
+    cmp al, 'Z'
+    jg .sig
+
+    add al, 32
+    mov [edx], al
+
+.sig:
+    inc edx
+    jmp .recorrer
+
+.fin:
+    pop edx
+    ret
+
+
 section .data
-    msg1 db "Ingresa una cadena: ", 0
-    nlin db 0x0A
+    msg1 db "Ingresa una cadena: ",0
+    msg2 db 0xA,"Original: ",0
+    msg3 db 0xA,"Mayusculas: ",0
+    msg4 db 0xA,"Minusculas: ",0
+
+    nlin db 0xA
+    len db 64
+
     cad times 64 db 0
