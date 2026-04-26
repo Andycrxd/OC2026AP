da45408c91bb02f0b7720561e6a1156250b1f5d0
Andy <andypalomares40@gmail.com>
2026-04-26 02:09:21 +0000

    salvar cambios

--- src/practica8/p8.asm
+++ src/practica8/p8.asm
@@ -1,25 +1,24 @@
 %include "../../lib/pc_io.inc"
 
+section .data
+    letra db 'A', 0      ; guardamos 'A' en memoria
+
 section .text
-global _start
+    global _start
 
 _start:
-    call getch        ; AL = tecla
-
-    mov bl, al        ;  guardamos la letra
-
-    ; imprimir letra
-    movzx edx, bl
+    ; Opción 1: pasar el código ASCII directamente
+    mov edx, 65          ; 65 = código ASCII de 'A'
     call putchar
 
-    ; imprimir espacio
-    mov edx, '-'
+    ; Opción 2: pasar el carácter como constante
+    mov edx, 'B'         ; NASM convierte 'B' a su valor ASCII (66)
     call putchar
 
-    ; imprimir letra otra vez
-    movzx edx, bl
+    ; Opción 3: leer el valor desde memoria
+    movzx edx, byte [letra]   ; lee 1 byte de 'letra' y lo pone en EDX
     call putchar
 
     mov eax, 1
-	mov ebx,0
+    mov ebx, 0
     int 0x80
