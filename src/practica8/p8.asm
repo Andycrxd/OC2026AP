ae02bbeecdda8e521c4aa2c448c3596f5b31071f
Andy <andypalomares40@gmail.com>
2026-04-26 02:22:44 +0000

    salvar cambios

--- src/practica8/p8.asm
+++ src/practica8/p8.asm
@@ -1,22 +1,22 @@
 %include "../../lib/pc_io.inc"
 
 section .data
-    letra db 'A', 0      ; guardamos 'A' en memoria
+    letra db 'A', 0
 
 section .text
     global _start
 
 _start:
-    ; Opción 1: pasar el código ASCII directamente
-    mov edx, 65          ; 65 = código ASCII de 'A'
+    mov edx, 65
     call putchar
 
-    ; Opción 2: pasar el carácter como constante
-    mov edx, 'B'         ; NASM convierte 'B' a su valor ASCII (66)
+    mov edx, 'B'
     call putchar
 
-    ; Opción 3: leer el valor desde memoria
-    movzx edx, byte [letra]   ; lee 1 byte de 'letra' y lo pone en EDX
+    movzx edx, byte [letra]
+    call putchar
+
+    mov edx, 10         ; ← '\n' fuerza flush del buffer ⭐
     call putchar
 
     mov eax, 1
