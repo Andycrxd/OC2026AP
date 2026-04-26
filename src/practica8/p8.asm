5fb35fa281cbec3a4ea090678eab17a6cc5a8678
Andy <andypalomares40@gmail.com>
2026-04-26 01:11:35 +0000

    salvar cambios

---
+++ src/practica8/p8.asm
@@ -0,0 +1,25 @@
+%include "../../lib/pc_io.inc"
+
+section .text
+global _start
+
+_start:
+    call getch        ; AL = tecla
+
+    mov bl, al        ;  guardamos la letra
+
+    ; imprimir letra
+    movzx edx, bl
+    call putchar
+
+    ; imprimir espacio
+    mov edx, ' '
+    call putchar
+
+    ; imprimir letra otra vez
+    movzx edx, bl
+    call putchar
+
+    mov eax, 1
+	mov ebx,0
+    int 0x80
