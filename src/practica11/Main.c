4a77477d315ca78a2c1e3f60b8caadf671dcc6e6
Andy <andypalomares40@gmail.com>
2026-05-15 06:16:32 +0000

    salvar cambios

--- src/practica11/Main.c
+++ src/practica11/Main.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
 extern int _fun(int, int);
-
+extern int mul(int,int);
+extern int sumamacro(int*,int);
 
 int main (void) 
 {
-  
+    int a=5;
+    int b=3;
+
+    int arreglo[5] ={1,1,2,3,5};
+
     printf("\n\rSuma de dos numeros %d\n ", _fun(8,2));
+    printf("\n\rLa multiplicaciopn de %d x %d = %d \n",a,b,mul(a,b));
+
+    printf("\n\rLa sumatoria del arreglo es %d\n",sumamacro(arreglo,5)); 
+    
     return 0;
 }
 
--- src/practica11/pppp8.asm
+++ src/practica11/pppp8.asm
@@ -3,8 +3,8 @@
 section .text
 
 global _fun
-
-
+global mul
+global sumamacro
 _fun:
     push ebp
     mov ebp,esp
@@ -19,6 +19,83 @@ _fun:
     mov esp,ebp
     pop ebp
     ret
+
+
+mul:
+    push ebp
+    mov ebp,esp
+
+    mov eax, [ebp+8]
+    mov edx, [ebp+12]
+
+    imul eax,edx
+
+
+
+    mov esp,ebp
+    pop ebp
+    ret
+
+
+%macro suM 2
+
+    push edx
+    push esi
+    push ecx
+    push ebx
+
+    mov eax,0
+    mov esi,0
+    mov ecx,0
+
+    mov ebx,%1
+    mov edx,%2
+
+
+    ciclo:
+        cmp edx,esi
+        je salir
+
+        movzx ecx,byte[ebx+esi*4]
+
+        add eax,ecx
+        inc esi
+
+        jmp ciclo
+
+
+    salir:
+
+    pop ebx
+    pop ecx
+    pop esi
+    pop edx
+
+%endmacro
+
+sumamacro:
+
+    push ebp
+    mov ebp,esp
+
+    
+    suM [ebp+8],[ebp+12]
+
+
+    
+    mov esp,ebp
+    pop ebp
+    ret
+
+
+
+
+
+
+
+
+
+
 
 
 
